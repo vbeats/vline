039fd860a4866417edacef1b786fe754a3850859
bootvue <bootvue@gmail.com>
2025-09-19 17:23:51 +0800

    watch event test

--- examples/vline-example-etcd/src/main/java/com/codestepfish/vline/example/Test.java
+++ examples/vline-example-etcd/src/main/java/com/codestepfish/vline/example/Test.java
@@ -1,10 +1,13 @@
 package com.codestepfish.vline.example;
 
+import cn.hutool.core.thread.ThreadUtil;
 import com.codestepfish.vline.etcd.EtcdClientHolder;
 import io.etcd.jetcd.ByteSequence;
 import io.etcd.jetcd.Client;
 import io.etcd.jetcd.KV;
+import io.etcd.jetcd.Watch;
 import io.etcd.jetcd.kv.GetResponse;
+import io.etcd.jetcd.watch.WatchResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +29,25 @@ public class Test {
 
         // put the key-value
         try {
+
+            Watch watchClient = client.getWatchClient();
+            Watch.Watcher watcher = watchClient.watch(ByteSequence.from("test_key".getBytes()), new Watch.Listener() {
+                @Override
+                public void onNext(WatchResponse response) {
+                    log.info("watch listener res: {}", response);
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    log.error("watch error", throwable);
+                }
+
+                @Override
+                public void onCompleted() {
+                    log.info("watch completed");
+                }
+            });
+
             kvClient.put(key, value).get();
 
             // get the CompletableFuture
@@ -35,9 +57,12 @@ public class Test {
             GetResponse response = getFuture.get();
 
             log.info("etcd res: {}", response);
-        } catch (Exception e) {
+        } catch (
+                Exception e) {
             log.error("etcd error", e);
         }
+
+        ThreadUtil.safeSleep(5000L);
 
         return "success";
     }
